c5b8bf7826c1a415fb3003b09bed2dad6199c553
Alawapr <93432929+Alawapr@users.noreply.github.com>
2024-06-05 21:40:51 +0200

    tauri init

---
+++ src/app_data.rs
@@ -0,0 +1,7 @@
+pub const MAIN_WINDOW: &str = "main";
+pub const APPS_DIR: &str = "web2app_apps";
+
+// following const may/may not be replaced by web2app cli
+pub const USER_AGENT: Option<&str> = Some("Mozilla Chrome Safari");
+pub const APP_NAME: &str = "ytmusic-rs";
+pub const URL: &str = "https://music.youtube.com/";
